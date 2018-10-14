2487a4ddad1d00185dae44731ae4993b03fc3bd4
Joel Cardinal <jcardinal@cpocommerce.com>
2018-10-14 00:35:29 -0400

    converted some  'for in' to 'for of' removed hardcoded paths extended config file added plugin system so that generator won't be site specific added package.json script.test put HTML meta tags in ojb so arbitrary data can be added

--- README.md
+++ README.md
@@ -2,4 +2,23 @@
 
 Yes, yet another static site generator. This is a Node based generator, the goal is to keep super small one page script, low/no dependancies, and easy to run.
 
-So far less than 300 lns and runs with ```node index.js```
+The script is only opinionated in the templating markup (double curly braces), and existance of certain file paths (although directory and file names can be your own).
+
+Directories to be copied (e.g. js, images) are required to be in your input directory.
+
+Script also allows "plugins" or really just includes modules that are passed that app, so your code can take advatange of existing methods.
+
+Plugin should return either null, name of page file, array of page file names that should be skipped for processing.  This would be useful for example when you have a plugin for page file(s) that won't be output itself but serves as a template for your own processing; maybe it's used to create a multitude of similar pages.
+
+## Requirements
+configs.json file located in parent diretory of generator repo directory. File must have valid directory paths even if they contain no files (see zipped example project).
+
+Your htmlTop file must contain the following template replacement strings:
+```
+{{topMetaTags}}
+{{topCSS}}
+{{topJS}}
+```
+
+## To Run
+Simply run: ```node index.js```
--- index.js
+++ index.js
@@ -1,208 +1,254 @@
 var fs = require("fs"),
   path = require("path"),
   execSync = require("child_process").execSync,
-  outputPath = "./output",
-  copyFolders = ["images", "css", "js"];
+  externalConfigs = require("../configs.json");
 
 // IMPORTANT: requires all file names are unique
 
-// TODO: lots of 'for in' that should or could be 'for of'
-// TODO: remove hardcoded paths and use config file
-// TODO: add plugin system so that generator won't be site specific
-// TODO: add npm hook to run local server for testing output
-// TODO: put HTML <meta in array so arbitrary data can be added
-
 (function(){
-  var cssArr=[],
-    js = {top:[],bottom:[]},
-    defaultMetaData = {
-      "baseURL" : "https://example.com",
-      "lang": "en",
-      "title": "Index",
-      "charset": "utf-8",
-      "description": "This is a page",
-      "keywords": "page, sample",
-      "author": "None"
+
+  var app = {
+    data : {
+      cssArr : [],
+      js : {
+        top:[],
+        bottom:[]
+      },
+      configs : {},
+      pagesToSkip : []
     },
-    htmlMetaData = defaultMetaData;
 
-  function getHtmlTop(){
-    var page = fs.readFileSync("./input/components/htmlTop.html", "utf8");
-    htmlMetaData.topJSHtml = getJSHtml("top");
-    htmlMetaData.cssHtml = getCssHtml();
-    function replacer() {
-      var content = htmlMetaData[arguments[1].trim()];
-      return content ? content : "";
-    }
-    return page.replace(/{{([^}}]+)?}}/g, replacer);
-  }
+    init : function(){
+      app.data.configs = externalConfigs;
+      app.processPlugins();
+      app.deleteOutput();
+      app.processPages();
+      app.createSitemap();
+      app.copyFilesToOutput();
+      // Process complete.
+      console.log("Done!");
+    },
 
-  function getHeaderFooter(type, pageData){
-    var headOrFoot = pageData[type],
-      header = "";
-    if(headOrFoot){
-      var fileName = /\.html/.test(headOrFoot) ? headOrFoot : headOrFoot+".html";
-      var data = "";
-      header = fs.readFileSync(path.join("./input/headers", fileName), "utf8");
-      data = getDataObj(header);
-      header = getComponents(removeDataString(header));
-      if(data){
-        addJS(data);
-        addCSS(data);
-        setHtmlMetaData(data);
+    processPlugins : function (){
+      var plugins = fs.readdirSync(app.data.configs.filePaths.plugins, "utf8");
+      for (var plugin of plugins){
+        var pluginScript = require(path.join(app.data.configs.filePaths.plugins,plugin));
+        var toSkip = pluginScript(app);
+        if(toSkip){
+          if(Array.isArray(toSkip)){
+            for(var name of toSkip){
+              app.data.pagesToSkip.push(name);
+            }
+          }else{
+            app.data.pagesToSkip.push(toSkip);
+          }
+        }
       }
-    }
-    return header;
-  }
-  function setHtmlMetaData(data){
-    for(i in data){
-      if(htmlMetaData[data[i]]){
-        htmlMetaData[data[i]] = data[i];
+    },
+
+    getHtmlTop : function(){
+      var page = fs.readFileSync(app.data.configs.filePaths.htmlTop, "utf8");
+      // silly to add to metaData, but efficient
+      app.data.configs.metaData.topJS = app.getJSHtml("top");
+      app.data.configs.metaData.topCSS = app.getCssHtml();
+      app.data.configs.metaData.topMetaTags = app.getMetaTags();
+      function replacer() {
+        var content = app.data.configs.metaData[arguments[1].trim()];
+        return content ? content : "";
       }
-    }
-  }
+      return page.replace(/{{([^}}]+)?}}/g, replacer);
+    },
 
-  function getJSHtml(type){
-    var scripts = "";
-    var jsArrReversed = js[type].reverse();
-    for(var i in jsArrReversed){
-      var fileName = jsArrReversed[i];
-      fileName = /\.js/.test(fileName) ? fileName : fileName+".js";
-      scripts += '<script src="js/'+fileName+'"></script>';
-    }
-    return scripts;
-  }
+    getHeaderFooter : function(type, pageData){
+      var headOrFoot = pageData[type],
+        header = "";
+      if(headOrFoot){
+        var fileName = /\.html/.test(headOrFoot) ? headOrFoot : headOrFoot+".html";
+        var data = "";
+        header = fs.readFileSync(path.join(app.data.configs.filePaths[type], fileName), "utf8");
+        data = app.getDataObj(header);
+        header = app.getComponents(app.removeDataString(header));
+        if(data){
+          app.addJS(data);
+          app.addCSS(data);
+          app.setConfigs(data);
+        }
+      }
+      return header;
+    },
 
-  function getCssHtml(){
-    var cssIncludes = "";
-    var cssArrReversed = cssArr.reverse();
-    for(var i in cssArrReversed){
-      var fileName = cssArrReversed[i];
-      fileName = /\.css/.test(fileName) ? fileName : fileName+".css";
-      cssIncludes += '<link rel="styleguide" href="css/'+fileName+'"></link>';
-    }
-    return cssIncludes;
-  }
+    setConfigs : function (data){
+      for(var dataKey in data.metaData){
+        app.data.configs.metaData[dataKey] = data.metaData[dataKey];
+      }
+      for(var tagKey in data.metaTags){
+        app.data.configs.metaTags[tagKey] = data.metaTags[tagKey];
+      }
+    },
 
-  function removeDataString(fileConents){
-    return fileConents.replace(/<<([^>>]+)?>>/,"").trim();
-  }
+    getMetaTags : function(){
+      var html = "";
+      for(var key in app.data.configs.metaTags){
+        html += `<meta name="${key}" content="${app.data.configs.metaTags[key]}"></meta>`;
+      }
+      return html;
+    },
 
-  function getDataObj(fileContents){
-    try{
-      fileContents =  fileContents.match(/<<([^>>]+)?>>/);
-      return fileContents && fileContents.length > 1 ? JSON.parse(fileContents[1]) : null;
-    }catch(err){
-      console.log("Error getDataObj(): "+err);
-      console.log(fileContents);
-    }
-  }
+    getJSHtml : function(type){
+      var scripts = "";
+      var jsArrReversed = app.data.js[type].reverse();
+      for(var fileName of jsArrReversed){
+        fileName = /\.js/.test(fileName) ? fileName : fileName+".js";
+        scripts += '<script src="js/'+fileName+'"></script>';
+      }
+      return scripts;
+    },
 
-  function getComponents (page){
-    function replacer() {
-      var component = "";
-      var componentName = arguments[1].trim();
-      componentName = /^c-/.test(componentName) ? componentName.replace("c-","") : componentName;
-      componentName = /\.html/.test(componentName) ? componentName : componentName+".html";
+    getCssHtml : function(){
+      var cssIncludes = "";
+      var cssArrReversed = app.data.cssArr.reverse();
+      for(var fileName of cssArrReversed){
+        fileName = /\.css/.test(fileName) ? fileName : fileName+".css";
+        cssIncludes += '<link rel="styleguide" href="css/'+fileName+'"></link>';
+      }
+      return cssIncludes;
+    },
+
+    removeDataString : function(fileConents){
+      return fileConents.replace(/<<([^>>]+)?>>/,"").trim();
+    },
+  
+    getDataObj : function(fileContents){
       try{
-        component = fs.readFileSync(path.join("./input/components",componentName), "utf8");
-        var componentData = getDataObj(component);
-        addJS(componentData);
-        addCSS(componentData);        
-        component = removeDataString(component);
+        fileContents =  fileContents.match(/<<([^>>]+)?>>/);
+        return fileContents && fileContents.length > 1 ? JSON.parse(fileContents[1]) : null;
       }catch(err){
-        console.log("Error getComponents("+componentName+"): "+err);
+        console.log("Error getDataObj(): "+err);
+        console.log(fileContents);
       }
-      return component;
-    }
-    return page.replace(/{{([^}}]+)?}}/g, replacer);
-  }
+    },
+  
+    getComponents : function (page){
+      function replacer() {
+        var component = "";
+        var componentName = arguments[1].trim();
+        componentName = /^c-/.test(componentName) ? componentName.replace("c-","") : componentName;
+        componentName = /\.html/.test(componentName) ? componentName : componentName+".html";
+        try{
+          component = fs.readFileSync(path.join(app.data.configs.filePaths.components,componentName), "utf8");
+          var componentData = app.getDataObj(component);
+          app.addJS(componentData);
+          app.addCSS(componentData);        
+          component = app.removeDataString(component);
+        }catch(err){
+          console.log("Error getComponents("+componentName+"): "+err);
+        }
+        return component;
+      }
+      return page.replace(/{{([^}}]+)?}}/g, replacer);
+    },
+  
+    addJS : function(data){
+      if(data.js && data.js.top.length){
+        for(var y in data.js.top){
+          app.data.js.top.push(data.js.top[y]);
+        }
+      }
+      if(data.js && data.js.bottom.length){
+        for(var z in data.js.bottom){
+          app.data.js.bottom.push(data.js.bottom[z]);
+        }
+      }
+    },
+  
+    addCSS : function(data){
+      if(data.css && data.css.length){
+        for(var w in data.css){
+          app.data.cssArr.push(data.css[w]);
+        }
+      }
+    },
 
-  function addJS(data){
-    if(data.js && data.js.top.length){
-      for(var y in data.js.top){
-        js.top.push(data.js.top[y]);
+    deleteOutput : function(){
+      // First delete everything in the build directory.
+      console.log("Cleaning previous build...");
+      try {
+        // rm -rf is always dangerous but much simpler than node here
+        var outputPath = app.data.configs.filePaths.output;
+        if(fs.existsSync(outputPath)){
+          execSync(`rm -rf ${outputPath} && mkdir ${outputPath}`);
+        }else{
+          console.log(`Error during cleanup: directory ${outputPath} not found`);
+        }
+      }catch(err){
+        console.log("Error during cleanup: "+err);
+        process.exit(1);
+      }
+    },
+
+    processPages : function(){
+      // handle each page
+      var pageFiles = fs.readdirSync(app.data.configs.filePaths.pages,"utf8");
+      for (var fileName of pageFiles){
+        // Ignore any pages flagged to skip by plugins
+        if(app.data.pagesToSkip.indexOf(/\.html/.test(fileName) ? fileName : fileName + ".html") > -1 ||
+          app.data.pagesToSkip.indexOf(/\.html/.test(fileName) ? fileName.replace(".html","") : fileName) > -1
+        ){continue;}
+        console.log("Adding page: "+fileName);
+        // TODO: this assumes page directory has no sub-directories
+        var page = fs.readFileSync(path.join(app.data.configs.filePaths.pages,fileName), "utf8");
+        var pageData = app.getDataObj(page);
+        var header = "";
+        var footer = "";
+        page = app.getComponents(app.removeDataString(page));
+        if(pageData){
+          header = app.getHeaderFooter("headers",pageData);
+          footer = app.getHeaderFooter("footers",pageData);
+          app.addJS(pageData);
+          app.addCSS(pageData);
+          app.setConfigs(pageData);
+        }
+        page = app.getHtmlTop() + header + page + footer + app.getJSHtml('bottom') + "</body></html>";
+        fs.writeFileSync(path.join(app.data.configs.filePaths.output,fileName),page);
+        // reset data for next page
+        app.data.js = {top:[],bottom:[]};
+        app.data.cssArr = [];
+        app.data.configs = externalConfigs;
+      }
+    },
+
+    createSitemap : function(){
+      // sitemap
+      console.log("Creating sitemap...");
+      var pageNames = fs.readdirSync(app.data.configs.filePaths.output,"utf8");
+      var sitemap = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">'+
+        "<url><loc>"+app.data.configs.baseURL+"/"+
+        pageNames.reverse().join("</loc><changefreq>weekly</changefreq></url><url><loc>"+app.data.configs.baseURL+"/")+
+        "</loc><changefreq>weekly</changefreq></url></urlset>";
+        // TODO: this assumes page directory has no sub-directories 
+      fs.writeFileSync(path.join(app.data.configs.filePaths.output,"sitemap.xml"), sitemap.replace("index.html",""));
+    },
+
+    copyFilesToOutput : function(){
+      // Copy asset folders into build folder.
+      // TODO: concat/compress assets
+      console.log("Copying folders...");
+      try {
+        for(var copyFolder of app.data.configs.copyFolders){
+          var src= path.join(app.data.configs.filePaths.input,copyFolder);
+          var dist= path.join(app.data.configs.filePaths.output,copyFolder);
+          execSync(`cp -r ${src} ${dist}`);
+          // Alternative depending on how cp -r works on different OS's
+          // execSync(`mkdir -p ${dist} && cp -r ${src} ${dist}`);
+        }
+      }catch(err){
+        console.log("Error during folder copying: "+err);
+        process.exit(1);
       }
     }
-    if(data.js && data.js.bottom.length){
-      for(var z in data.js.bottom){
-        js.bottom.push(data.js.bottom[z]);
-      }
-    }
-  }
 
-  function addCSS(data){
-    if(data.css && data.css.length){
-      for(var w in data.css){
-        cssArr.push(data.css[w]);
-      }
-    }
-  }
+  }; // end app
 
-  // First delete everything in the build directory.
-  console.log("Cleaning previous build...");
-  try {
-    // rm -rf is always dangerous but much simpler than node here
-    // hardcoding "output" to make sure command has specific target,
-    // instead of deleting hd
-    execSync(`rm -rf output && mkdir output`);
-  }catch(err){
-    console.log("Error during cleanup: "+err);
-    process.exit(1);
-  }
-
-  // handle each page
-  var pageFiles = fs.readdirSync("./input/pages","utf8");
-  for (var i in pageFiles){
-    var fileName = pageFiles[i];
-    console.log("Adding page: "+fileName);
-    // TODO: this assumes page directory has no sub-directories
-    var page = fs.readFileSync(path.join("./input/pages",fileName), "utf8");
-    var pageData = getDataObj(page);
-    var header = "";
-    var footer = "";
-    page = getComponents(removeDataString(page));
-    if(pageData){
-      header = getHeaderFooter("header",pageData);
-      footer = getHeaderFooter("footer",pageData);
-      addJS(pageData);
-      addCSS(pageData);
-      setHtmlMetaData(pageData);
-    }
-    page = getHtmlTop() + header + page + footer + getJSHtml('bottom') + "</body></html>";
-    fs.writeFileSync(path.join(outputPath,fileName),page);
-    // reset data for next page
-    js = {top:[],bottom:[]};
-    cssArr = [];
-    htmlMetaData = defaultMetaData;
-  }
-
-  // sitemap
-  console.log("Creating sitemap...");
-  var pageNames = fs.readdirSync(outputPath,"utf8");
-  var sitemap = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">'+
-    "<url><loc>"+htmlMetaData.baseURL+"/"+
-    pageNames.reverse().join("</loc><changefreq>weekly</changefreq></url><url><loc>"+htmlMetaData.baseURL+"/")+
-    "</loc><changefreq>weekly</changefreq></url></urlset>";
-  fs.writeFileSync(path.join(outputPath,"sitemap.xml"), sitemap.replace("index.html",""));
-
-  // Copy asset folders into build folder.
-  // TODO: concat/compress assets
-  console.log("Copying folders...");
-  try {
-    for(var copyFolder of copyFolders){
-      var src= path.join("./input",copyFolder);
-      var dist= path.join(outputPath,copyFolder);
-      execSync(`cp -r ${src} ${dist}`);
-      // Alternative depending on how cp -r works on different OS's
-      // execSync(`mkdir -p ${dist} && cp -r ${src} ${dist}`);
-    }
-  }catch(err){
-    console.log("Error during folder copying: "+err);
-    process.exit(1);
-  }
-
-  // Process complete.
-  console.log("Done!");
+  app.init();
 
 })();
